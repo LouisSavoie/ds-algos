a4bd2c4dd10b3b37b04d7ff81cfd2661f7f7c609
Louis Savoie <thelousavoie@gmail.com>
2021-04-01 18:17:40 -0400

    add sum array

---
+++ ruby/sum_array.rb
@@ -0,0 +1,13 @@
+# Sum an array.
+# Time Complexity: O(n), linear
+
+def sum(array)
+    sum = 0
+    for x in array do
+        sum += x
+    end
+    return sum
+end
+
+nums = [1,2,3]
+puts "sum is: #{sum(nums)}"
