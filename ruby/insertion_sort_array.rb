813a0ea40d35fca71f8b39f10139ae510ae0d412
Louis Savoie <thelousavoie@gmail.com>
2021-04-04 22:04:45 -0400

    add some comments

--- ruby/insertion_sort_array.rb
+++ ruby/insertion_sort_array.rb
@@ -5,12 +5,15 @@ def sort(array)
     array[1..-1].each do |x|
         while x < array[array.index(x) - 1] && array.index(x) != 0
             array.insert(array.index(x) - 1, array.delete(x))
-            # test: puts "x = #{x}, index = #{array.index(x)}"
+            #test: puts "x = #{x}, index = #{array.index(x)}"
         end
     end
     return array
 end
 
+# Test Data
 nums = [5,2,4,6,1,3]
+
+# Tests
 puts "sorted:   #{sort(nums)}"
 puts "expected: [1, 2, 3, 4, 5, 6]"
--- ruby/sum_recursive_array.rb
+++ ruby/sum_recursive_array.rb
@@ -1,10 +1,13 @@
-# Sum an array.
+# Sum an array recursivly.
 # Time Complexity: 
 
 def sum(array)
     array.length() === 0 ? (return 0) : (return array[0] + sum(array[1..-1]))
 end
 
+# Test Data
 nums = [1,2,3]
+
+# Tests
 puts "sum is:   #{sum(nums)}"
 puts "expected: 6"
